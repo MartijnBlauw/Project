ecbdbdbb2455987ed192136402534c682a6bc6ea
MartijnBlauw <martijnblauw@github.com>
2018-01-15 11:43:41 +0100

    Eerste keer met project

---
+++ BoozeYourCredit/LoginViewController.swift
@@ -0,0 +1,88 @@
+//
+//  LoginViewController.swift
+//  BoozeYourCredit
+//
+//  Created by Martijn Blauw on 11-01-18.
+//  Copyright © 2018 Martijn Blauw. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+
+    // Outlets textfields
+    @IBOutlet weak var textFieldLoginEmail: UITextField!
+    @IBOutlet weak var textFieldLoginPassword: UITextField!
+    
+    // Register
+    @IBAction func registerButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Register",
+                                      message: "Please enter your email and password",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            let emailField = alert.textFields![0]
+            let passwordField = alert.textFields![1]
+
+            Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { user, error in
+                if error == nil {
+                    Auth.auth().signIn(withEmail: self.textFieldLoginEmail.text!, password: self.textFieldLoginPassword.text!)
+                }
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField { textEmail in
+            textEmail.placeholder = "Enter your email"
+        }
+        
+        alert.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // Login
+    @IBAction func loginButton(_ sender: UIButton) {
+        Auth.auth().signIn(withEmail: textFieldLoginEmail.text!,
+                           password: textFieldLoginPassword.text!)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Auth.auth().addStateDidChangeListener() { auth, user in
+            if user != nil {
+                self.performSegue(withIdentifier: "LoginSegue", sender: nil)
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == textFieldLoginPassword {
+            textFieldLoginPassword.becomeFirstResponder()
+        }
+        if textField == textFieldLoginPassword {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+}
+
+
